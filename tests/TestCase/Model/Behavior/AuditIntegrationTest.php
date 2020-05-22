d68c806f9a22f349f81552b4eb144b91a23067fb
Ishan Vyas <isvyas@gmail.com>
2020-05-22 18:14:23 +0530

    Add test

--- tests/TestCase/Model/Behavior/AuditIntegrationTest.php
+++ tests/TestCase/Model/Behavior/AuditIntegrationTest.php
@@ -371,6 +371,7 @@ class AuditIntegrationTest extends TestCase
     public function testDelete()
     {
         $entity = $this->table->get(1);
+
         $this->persister
             ->expects($this->once())
             ->method('logEvents')
@@ -380,6 +381,7 @@ class AuditIntegrationTest extends TestCase
                 $this->assertEquals(1, $events[0]->getId());
                 $this->assertEquals('articles', $events[0]->getSourceName());
                 $this->assertNotEmpty($events[0]->getTransactionId());
+                $this->assertNotEmpty($events[0]->getOriginal());
             }));
 
         $this->table->delete($entity);
