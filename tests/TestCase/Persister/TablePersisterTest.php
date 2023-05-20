5c2167e0349dad07d6a5435e65b4ea9f41457a64
chris cnizzardini <cnizzardini@gmail.com>
2023-05-20 12:45:24 -0400

    resolve timezone bug

--- tests/TestCase/Persister/TablePersisterTest.php
+++ tests/TestCase/Persister/TablePersisterTest.php
@@ -305,7 +305,6 @@ class TablePersisterTest extends TestCase
 
     public function testErrorLogging()
     {
-        $this->markTestSkipped();
         $event = new AuditCreateEvent('62ba2e1e-1524-4d4e-bb34-9bf0e03b6a96', 1, 'source', [], []);
 
         /* @var $TablePersister TablePersister|\PHPUnit_Framework_MockObject_MockObject */
