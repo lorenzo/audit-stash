34bfd69b32bcc990daddb3a4333fc9eaf1f0f9b5
chris cnizzardini <cnizzardini@gmail.com>
2023-05-20 12:35:30 -0400

    resolve timezone bug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,5 +22,7 @@ require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
 use Cake\Datasource\ConnectionManager;
 
+date_default_timezone_set('UTC');
+
 // Connection for audit storage
 ConnectionManager::setConfig('test_elastic', ['url' => getenv('elastic_dsn')]);
