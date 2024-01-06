b31b02868a26596f49b728f1e270bcdc6da76f78
Chris Nizzardini <chris.nizzardini@thecmigroupa.ca>
2024-01-05 20:19:57 -0500

    WIP

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.0.1
- * @license   https://www.opensource.org/licenses/mit-license.php MIT License
- */
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 define('CAKE', dirname(__DIR__) . '/vendor/cakephp/cakephp/src/');
